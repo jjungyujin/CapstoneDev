0223cdcad4a8bfb77b07c75acf33378369604546
seong woo <jsw6872@gmail.com>
2022-12-06 20:26:00 +0900

    update utils file

--- backend/utils.py
+++ backend/utils.py
@@ -2,3 +2,21 @@ columns = ['PRC_CD', 'TIME', 'COIL_NO', 'COIL_NO_TF', 'PASS_NO', 'PASS_NO_TF', '
 
 thk_colums = ['TIME','COIL_NO', 'PASS_NO', 'EXIT_THK_ACT_AVG']
 jaryo_columns = ['2CR_IBA_DB230_REAL148', '2CR_IBA_DB230_REAL156', '2CR_IBA_DB230_REAL164', '2CR_IBA_DB340_REAL52', '2CR_IBA_DB500_REAL24', '2CR_IBA_DB500_REAL48', '2CR_IBA_DB500_REAL88', 'N2_AGC_DB222_DD154', 'N2_CPU2_DB801_DD142', 'N2_CPU2_DB801_DD278', 'N2_CPU1_DB801_DD42', 'N2_CPU1_DB801_DD46']
+
+rename_dict = {'EXIT_THK_ACT_AVG' : 'ENTRY_THK_ACT_AVG',
+               '2CR_IBA_DB230_REAL148' : 'upper_roll_shift_set',
+               '2CR_IBA_DB230_REAL156' : 'lower_roll_shift_set',
+               '2CR_IBA_DB230_REAL164' : 'bending_DS_set',
+               '2CR_IBA_DB500_REAL24' : 'LH_tension_set',
+               '2CR_IBA_DB500_REAL48' : 'RH_tension_set',
+               '2CR_IBA_DB500_REAL88' : 'POR_tension_set',
+               'N2_AGC_DB222_DD154' : 'roll_force_set',
+               'N2_CPU2_DB801_DD142' : 'speed_set',
+               'N2_CPU2_DB801_DD278' : 'POR_tension_N/mm2_set',
+               'N2_CPU1_DB801_DD42' : 'left_cooler_amount',
+               'N2_CPU1_DB801_DD46' : 'right_cooler_amount',
+               'EXIT_THK_ACT_AVG_y' : 'EXIT_THK_ACT_AVG'}
+
+used_columns = ['ENTRY_THK_ACT_AVG', 'upper_roll_shift_set', 'lower_roll_shift_set', 'bending_DS_set',
+                     'LH_tension_set', 'RH_tension_set', 'POR_tension_set', 'POR_tension_N/mm2_set', 'roll_force_set', 'speed_set',
+                     'left_cooler_amount', 'right_cooler_amount', 'EXIT_THK_ACT_AVG']
