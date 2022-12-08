db8eaac953d132dca654a758e0d9a9593df2dee1
Yujin  Jung <wjd1dbwls@gmail.com>
2022-12-08 14:57:09 +0900

    front : main page header, nav, content

---
+++ frontend/src/components/mainHeader.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+    <!-- Navbar Brand-->
+    <a class="navbar-brand ps-3" href="/"
+      >Prediction System of Exit Thickness</a
+    >
+  </nav>
+</template>
