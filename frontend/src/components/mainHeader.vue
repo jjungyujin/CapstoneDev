133268a837a37c5332da7e7a5ba4f91294282048
yongchoooon <sod7050@gmail.com>
2022-12-12 16:16:24 +0900

    CSS: 네이비 색 #36

--- frontend/src/components/mainHeader.vue
+++ frontend/src/components/mainHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+  <nav class="sb-topnav navbar navbar-expand navbar-dark bg-white shadow-sm">
     <!-- Navbar Brand-->
     <a class="navbar-brand ps-3" href="/"
       >Prediction System of Exit Thickness</a
